5f6249aab07b38a7e8422a5a8499e43a58d8bb6e
Dimple <dsaraogi2000@gmail.com>
2023-11-06 15:13:16 +0545

    add signup and login routes using passport

--- schema.js
+++ schema.js
@@ -18,3 +18,17 @@ module.exports.reviewSchema = Joi.object({
     comment: Joi.string().required(),
   }).required(),
 });
+
+// module.exports.userSchema = Joi.object({
+//   user: Joi.object({
+//     username: Joi.string().alphanum().min(3).max(30).required(),
+//     email: Joi.string().required(),
+//     password: Joi.string().required(),
+//   }).required(),
+// });
+
+module.exports.userSchema = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
